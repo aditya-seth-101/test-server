0df30681b477b2a369188f3bf36e2542ca39ffd1
aditya-seth-202 <aditya.shethink@gmail.com>
2025-01-16 13:06:51 +0530

    added path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path"); // Import the path module
 
 const app = express();
 const server = http.createServer(app);
