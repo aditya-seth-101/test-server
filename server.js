5db37ab458c6bccd6073c86b4faa2f9f07814880
aditya-seth-202 <aditya.shethink@gmail.com>
2025-01-16 19:28:14 +0530

    added speed test fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
-    maxHttpBufferSize: 1e8, 
+    maxHttpBufferSize: 1e10, 
   },
 });
 
