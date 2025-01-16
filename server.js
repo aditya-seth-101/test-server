3869b2ce13a6d3c7b18335808a98407cb29c568a
aditya-seth-202 <aditya.shethink@gmail.com>
2025-01-16 12:50:04 +0530

    port changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-const port = process.env.PORT 
+const port = process.env.PORT  || 8000
 
 
 // Chaos function to simulate latency and packet loss
